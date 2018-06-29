15d9d34ddf70a7f6f868b2788220c6e2c520cd59
Ryusei Nomi <nomi.ryusei@hamee.co.jp>
2018-06-29 15:35:13 +0900

    [追加]画像の表示

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,24 +2,42 @@ $('.bigbanner_panel').remove();
 $('#player_bg_outer').css('margin-left', '50px');
 $('#player_bg_outer.anime_sw_right').css('right', '-290px');
 $('#body_main_inner').css('margin-left', '50px');
-$('.content').prepend("<div class=\"twitter-images\"><img src=\"http://placehold.jp/250x250.png\"></div>");
-$.getScript("../webRTC/app.js");
-//$.ajax({
-//    url: 'file://Users/nomi.ryusei/workspace/hackathon/anime-image-collection-server/webRTC/app.js',
-//    type: 'GET',
-//    dataType: 'json',
-//    // フォーム要素の内容をハッシュ形式に変換
-//    //data: $('form').serializeArray(),
-//    //timeout: 5000,
-//  })
-//  .done(function(data) {
-//      alert('ほげ');
-//  })
-//  .fail(function() {
-//      alert('ふが');
-//  });
+$('.content').prepend("<div class=\"twitter-images\" style=\"float: right;margin-right: 50px;width: 250px;height: 250px;\"></div>");
+
+$.ajax({
+    url: 'http://localhost:3000/samples', 
+    type: 'GET',
+    //dataType: 'json',
+    // フォーム要素の内容をハッシュ形式に変換
+    //data: $('form').serializeArray(),
+    //timeout: 5000,
+  })
+  .done(function(data) {
+      console.log(data);
+      var l = data.length;
+      for(var i=0; i<l; i++){
+          console.log("<a href=\"" + data[i] + "\" download><img src=\"" + data[i] + "\" style=\"width: 250px\" /></a>");
+          //$('.twitter-images').prepend("<a href=\"" + data[i] + "\" download=\"sample\"><img src=\"" + data[i] + "\" style=\"width: 250px\" alt=\"sample\" /></a>");
+          $('.twitter-images').prepend("<div class=\"image\"><a href=\"" + data[i] + "\"><img src=\"" + data[i] + "\" style=\"width: 250px\" /></a></div>");
+      }
+  })
+  .fail(function(data) {
+      alert('サーバとの接続に失敗しました');
+  });
 
 $('.twitter-images').css({
     'float': 'right',
     'margin-right': '50px'
 });
+
+//画像のDL
+//$('head').html(
+//'<script>\
+//$(function () {\
+//    $(".image").on("click", function(e){\
+//        window.open('https://pbs.twimg.com/media/Dg1IUfQU0AAfKko.jpg');
+//    });\
+//});\
+//</script>\
+//'
+//)
